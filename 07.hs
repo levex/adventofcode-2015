05713a355762df0af8182008ade6751ae5c06eb8
Levente Kurusa (LK1015) <levente.kurusa15@imperial.ac.uk>
2016-01-18 16:35:16 +0000

    07: add parsing
    Signed-off-by: Levente Kurusa (LK1015) <levente.kurusa15@imperial.ac.uk>

--- 07.hs
+++ 07.hs
@@ -2,13 +2,13 @@
 
 module SomeAssemblyRequired where
 
-import Control.Monad
-import Control.Monad.State
-import Control.Applicative
-import Control.Monad.Error.Class
-import Control.Monad.State.Class
-import Control.Monad.Trans.Except
-import Data.Monoid
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Except
+import           Control.Monad.State
+import           Data.Bits
+import           Data.Monoid
+import qualified Data.Map.Strict as M
 
 type Wire = String
 
@@ -19,7 +19,15 @@ data Connection
   | RShiftGate Wire Int  Wire
   | AndGate    Wire Wire Wire
   | OrGate     Wire Wire Wire
+  | ErrorConnection
+    deriving (Show)
 
+type MachineState = M.Map String Int
+
+executeConnection :: MachineState -> Connection -> MachineState
+executeConnection = undefined
+
+-- λ
 -- Parsing
 
 data ParserError
@@ -27,6 +35,7 @@ data ParserError
   | EOFError
   | SatisfyFailedError
   | CollectionError [ParserError]
+    deriving (Show)
 
 type ParserState = String
 
@@ -35,10 +44,18 @@ newtype Parser a =
               deriving (Functor, Monad, Applicative, Alternative,
                         MonadState ParserState, MonadError ParserError)
 
+runMonadic
+  = runState . runExceptT . runParser
+
+parse :: Parser a -> String -> Either ParserError a
+parse
+  = (.) fst . runMonadic
+
 instance Monoid ParserError where
   mempty = CollectionError []
   mappend (CollectionError e1) e2 = CollectionError (e2 : e1)
   mappend e2 (CollectionError e1) = CollectionError (e2 : e1)
+  mappend e1 e2                   = CollectionError [e1, e2]
 
 item :: Parser Char
 item = do
@@ -49,9 +66,69 @@ item = do
       put cs
       return c
 
+try :: Parser a -> Parser a
+try p = do
+  init <- get
+  p `catchError` \e -> do
+    put init
+    throwError e
+
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = do
+satisfy p = try $ do
   c <- item
   if p c then
     return c
   else throwError SatisfyFailedError
+
+char :: Char -> Parser Char
+char = satisfy . (==)
+
+(<:>) :: Parser a -> Parser [a] -> Parser [a]
+(<:>) = liftA2 (:)
+
+string :: String -> Parser String
+string str = do
+  case str of
+    [] -> return []
+    (c : cs) -> char c <:> string cs
+
+whitespace :: Parser String
+whitespace = many $ char ' '
+
+wsstring :: String -> Parser String
+wsstring str = whitespace *> string str
+
+digit :: Parser Char
+digit = satisfy (liftA2 (&&) (>= '0') (<= '9'))
+
+number :: Parser Int
+number = read <$> some digit
+
+alphabetic :: Parser Char
+alphabetic = satisfy (`elem` ['a'..'z'])
+
+value :: Parser Int
+value = whitespace *> number
+
+arrow :: Parser String
+arrow = whitespace *> string "->"
+
+wire :: Parser Wire
+wire = whitespace *> some alphabetic
+
+line :: Parser Connection
+line =   try (Value      <$> value <*  arrow <*> wire)
+     <|> try ((string "NOT") *> (NotGate    <$> wire  <*  arrow <*> wire))
+     <|> try ((LShiftGate <$> (try (wire <|> value) <* (wsstring "LSHIFT")) <*> value <* arrow <*> wire))
+     <|> try ((RShiftGate <$> (try (wire <|> value) <* (wsstring "RSHIFT")) <*> value <* arrow <*> wire))
+     <|> try ((AndGate    <$> (try (wire <|> value) <* (wsstring "AND"))    <*> wire  <* arrow <*> wire))
+     <|> try ((OrGate     <$> (try (wire <|> value) <* (wsstring "OR"))     <*> wire  <* arrow <*> wire))
+     <|> (throwError $ OtherError "Invalid input")
+
+parseFile :: String -> IO [Connection]
+parseFile file = do
+  c <- lines <$> readFile file
+  mapM (\e -> (f (parse line e))) c
+  where
+    f (Left _) = return ErrorConnection
+    f (Right s) = return s
